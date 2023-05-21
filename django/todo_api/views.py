ce053f1a9229265fced643d7b1906640645f0416
Abdulazeez Saheed <saheedazeez2003@gmail.com>
2023-05-21 08:19:00 +0100

    submission

--- django/todo_api/views.py
+++ django/todo_api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+   
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_url_kwarg = 'task_id'
+
+
+class TaskComplete(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_url_kwarg = 'task_id'
+
+    def perform_update(self, serializer):
+        serializer.completed = True
+        return serializer
